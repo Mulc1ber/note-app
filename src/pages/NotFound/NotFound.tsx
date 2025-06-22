c7ff198e9f6d7ce1668fbdd1ed38dbc574eebb0e
Mulc1ber <mike@m.ru>
2025-06-22 11:29:05 +0300

    chore: change text in notfound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -5,7 +5,7 @@ import styles from "./NotFound.module.css";
 export const NotFound: React.FC = () => {
   return (
     <div className={styles["not-found"]}>
-      <h1>Not Found</h1>
+      <h1>Страница не найдена</h1>
       <Link to="/">Вернуться на главную страницу</Link>
     </div>
   );
