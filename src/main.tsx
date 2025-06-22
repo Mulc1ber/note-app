5992b4d9fe3a2c9a99f42f261d6e7f0ab81abaae
Mulc1ber <mike@m.ru>
2025-06-22 13:14:06 +0300

    feate: create and setting PWA

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import "@mantine/core/styles.layer.css";
 import { App } from "./App.tsx";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("Service Worker registered", reg))
+    .catch((err) => console.log("Service Worker not registered", err));
+}
+
 createRoot(document.getElementById("root")!).render(
   <MantineProvider forceColorScheme="light">
     <ModalsProvider>
